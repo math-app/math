b8b30c45b5b15cb4b89a1ddb554e8d5eb0766277
root <root@debian.debian>
2015-03-09 14:28:20 -0300

    Templating

--- application.py
+++ application.py
@@ -3,12 +3,13 @@ from bottle import Bottle, route, run, template
 app = application = Bottle()
 
 @app.route('/')
-def hello():
-    return template('<b>Index {{teste}}</b>!', teste="page")
-
 @app.route('/hello/<name>')
-def hello(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+def hello(name = 'Stranger'):
+    return template('index', name=name)
+
+@app.error(404)
+def error404(error):
+    return '<img src="/static/images/nothing.jpg" />'
 
 if __name__ == '__main__':
     run(host='localhost', port=8080)
